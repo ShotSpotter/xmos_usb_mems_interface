cd3a8fc6f17d154d0357548e48f6be309905e864
murphey9 <murphey@murphstuff.com>
2022-05-15 22:07:46 -0400

    [SCPT-367] Update FIR coefficients for 48kHz audio

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2017, XMOS Ltd, All rights reserved
+// Copyright (c) 2022, XMOS Ltd, All rights reserved
 extern const int g_first_stage_fir_0[256];
 extern const int g_first_stage_fir_1[256];
 extern const int g_first_stage_fir_2[256];
@@ -10,22 +10,26 @@ extern const int fir2_debug[16];
 
 extern const int g_third_stage_div_2_fir[126];
 extern const int fir3_div_2_debug[64];
-#define FIR_COMPENSATOR_DIV_2 (370581495)
+#define FIR_COMPENSATOR_DIV_2 (400681617)
 
 extern const int g_third_stage_div_4_fir[252];
 extern const int fir3_div_4_debug[128];
-#define FIR_COMPENSATOR_DIV_4 (382396580)
+#define FIR_COMPENSATOR_DIV_4 (413564502)
 
 extern const int g_third_stage_div_6_fir[378];
 extern const int fir3_div_6_debug[192];
-#define FIR_COMPENSATOR_DIV_6 (381275902)
+#define FIR_COMPENSATOR_DIV_6 (412398188)
 
 extern const int g_third_stage_div_8_fir[504];
 extern const int fir3_div_8_debug[256];
-#define FIR_COMPENSATOR_DIV_8 (382489919)
+#define FIR_COMPENSATOR_DIV_8 (413728857)
 
 extern const int g_third_stage_div_12_fir[756];
 extern const int fir3_div_12_debug[384];
-#define FIR_COMPENSATOR_DIV_12 (383139007)
+#define FIR_COMPENSATOR_DIV_12 (414443270)
+
+extern const int g_third_stage_custom_wideband_fir[126];
+extern const int fir3_custom_wideband_debug[64];
+#define FIR_COMPENSATOR_CUSTOM_WIDEBAND (370682921)
 
 #define THIRD_STAGE_COEFS_PER_STAGE (32)
