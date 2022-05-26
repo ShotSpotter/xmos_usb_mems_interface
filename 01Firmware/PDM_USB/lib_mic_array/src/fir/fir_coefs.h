db7d1a3946d8c95de6db4b04143b9fe572a1cd98
Robert B. Calhoun <rcalhoun@shotspotter.com>
2022-05-25 21:40:58 -0400

    Update coefficients using current xmos defaults plus add a filter for 96 kHz using:
    ./fir_design.py --add-third-stage  1 20.0 40.0 div_1 32

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
@@ -3,33 +3,33 @@ extern const int g_first_stage_fir_0[256];
 extern const int g_first_stage_fir_1[256];
 extern const int g_first_stage_fir_2[256];
 extern const int fir1_debug[48];
-#define FIRST_STAGE_MAX_PASSBAND_OUTPUT (1523241349)
+#define FIRST_STAGE_MAX_PASSBAND_OUTPUT (2022233792)
 
 extern const int g_second_stage_fir[8];
 extern const int fir2_debug[16];
 
 extern const int g_third_stage_div_2_fir[126];
 extern const int fir3_div_2_debug[64];
-#define FIR_COMPENSATOR_DIV_2 (400681617)
+#define FIR_COMPENSATOR_DIV_2 (276212818)
 
 extern const int g_third_stage_div_4_fir[252];
 extern const int fir3_div_4_debug[128];
-#define FIR_COMPENSATOR_DIV_4 (413564502)
+#define FIR_COMPENSATOR_DIV_4 (285017600)
 
 extern const int g_third_stage_div_6_fir[378];
 extern const int fir3_div_6_debug[192];
-#define FIR_COMPENSATOR_DIV_6 (412398188)
+#define FIR_COMPENSATOR_DIV_6 (284180149)
 
 extern const int g_third_stage_div_8_fir[504];
 extern const int fir3_div_8_debug[256];
-#define FIR_COMPENSATOR_DIV_8 (413728857)
+#define FIR_COMPENSATOR_DIV_8 (285084376)
 
 extern const int g_third_stage_div_12_fir[756];
 extern const int fir3_div_12_debug[384];
-#define FIR_COMPENSATOR_DIV_12 (414443270)
+#define FIR_COMPENSATOR_DIV_12 (285567922)
 
-extern const int g_third_stage_custom_wideband_fir[126];
-extern const int fir3_custom_wideband_debug[64];
-#define FIR_COMPENSATOR_CUSTOM_WIDEBAND (370682921)
+extern const int g_third_stage_div_1_fir[63];
+extern const int fir3_div_1_debug[32];
+#define FIR_COMPENSATOR_DIV_1 (223651801)
 
 #define THIRD_STAGE_COEFS_PER_STAGE (32)
