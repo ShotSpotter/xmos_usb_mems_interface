8081b1564a8fd6310b6f90f85b75bdcc1efd75e9
robertbcalhoun <rcalhoun@shotspotter.com>
2022-06-02 22:02:20 -0400

    * Fix issue with 2nd stage passband = stopband * Overlap stages * Add custom third stage (must change code)
    python3 fir_design.py \
    --first-stage-pass-bw 24.0 \
    --first-stage-stop-bw 48.0 \
    --first-stage-stop-atten -95.0 \
    --second-stage-pass-bw 12.0 \
    --second-stage-stop-bw 24.0 \
    --second-stage-stop-atten -60.0 \
    --third-stage-num-taps 32 \
    --third-stage-stop-atten -70.0 \
    --add-third-stage 2 8.0 16.0 custom_div_2 32

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
@@ -3,33 +3,33 @@ extern const int g_first_stage_fir_0[256];
 extern const int g_first_stage_fir_1[256];
 extern const int g_first_stage_fir_2[256];
 extern const int fir1_debug[48];
-#define FIRST_STAGE_MAX_PASSBAND_OUTPUT (1834513415)
+#define FIRST_STAGE_MAX_PASSBAND_OUTPUT (1840405112)
 
 extern const int g_second_stage_fir[8];
 extern const int fir2_debug[16];
 
 extern const int g_third_stage_div_2_fir[126];
 extern const int fir3_div_2_debug[64];
-#define FIR_COMPENSATOR_DIV_2 (307700128)
+#define FIR_COMPENSATOR_DIV_2 (300982049)
 
 extern const int g_third_stage_div_4_fir[252];
 extern const int fir3_div_4_debug[128];
-#define FIR_COMPENSATOR_DIV_4 (317512629)
+#define FIR_COMPENSATOR_DIV_4 (310560636)
 
 extern const int g_third_stage_div_6_fir[378];
 extern const int fir3_div_6_debug[192];
-#define FIR_COMPENSATOR_DIV_6 (316582713)
+#define FIR_COMPENSATOR_DIV_6 (309631242)
 
 extern const int g_third_stage_div_8_fir[504];
 extern const int fir3_div_8_debug[256];
-#define FIR_COMPENSATOR_DIV_8 (317591199)
+#define FIR_COMPENSATOR_DIV_8 (310609643)
 
 extern const int g_third_stage_div_12_fir[756];
 extern const int fir3_div_12_debug[384];
-#define FIR_COMPENSATOR_DIV_12 (318130692)
+#define FIR_COMPENSATOR_DIV_12 (311131710)
 
-extern const int g_third_stage_div_1_fir[63];
-extern const int fir3_div_1_debug[32];
-#define FIR_COMPENSATOR_DIV_1 (249154851)
+extern const int g_third_stage_custom_div_2_fir[126];
+extern const int fir3_custom_div_2_debug[64];
+#define FIR_COMPENSATOR_CUSTOM_DIV_2 (258960416)
 
 #define THIRD_STAGE_COEFS_PER_STAGE (32)
