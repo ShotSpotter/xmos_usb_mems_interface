a1f57afa724aa3751ce3ebf5579456d03c1eb09d
Robert B. Calhoun <rcalhoun@shotspotter.com>
2022-11-02 16:49:22 -0400

    Make our factory image with DFU support version 2.0.0 to signal breaking compatibility

--- 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
+++ 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
@@ -102,7 +102,7 @@ void genclock();
 // BCD_DEVICE_J, BCD_DEVICE_M, BCD_DEVICE_N instead"
 
 // #define BCD_DEVICE              0x0001
-#define BCD_DEVICE_J            1
+#define BCD_DEVICE_J            2
 #define BCD_DEVICE_M            0
 #define BCD_DEVICE_N            0
 #define AUDIO_CLASS             2
