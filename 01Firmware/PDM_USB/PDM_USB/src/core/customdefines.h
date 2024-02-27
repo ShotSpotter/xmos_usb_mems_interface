392e81d1a715a3794d34c20c281b15e777ceb546
Robert B. Calhoun <rcalhoun@shotspotter.com>
2024-02-27 09:52:29 -0500

    bump rev to 2.4.1 to identify dfu version. No functional changes

--- 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
+++ 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
@@ -104,7 +104,7 @@ void genclock();
 // #define BCD_DEVICE              0x0001
 #define BCD_DEVICE_J            2
 #define BCD_DEVICE_M            4
-#define BCD_DEVICE_N            0
+#define BCD_DEVICE_N            1
 #define AUDIO_CLASS             2
 #define AUDIO_CLASS_FALLBACK    0
 
