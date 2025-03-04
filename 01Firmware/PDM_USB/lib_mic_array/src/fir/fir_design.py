08889591b056d6e8c7f9a92b41b6df36ace7cb53
Robert B. Calhoun <rcalhoun@shotspotter.com>
2025-03-04 16:12:49 -0500

    Revert "SCPT-507: build with 40 kHz low-pass for critical infrastructure"
    This reverts commit 2efd057c1e89fc3948d5d7258bbe3e7f1e82ba2a to return
    repo to standard filters for public safety

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_design.py
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_design.py
@@ -41,10 +41,10 @@ def parseArguments(third_stage_configs):
     parser.add_argument('--first-stage-stop-atten', type=float, default = -80.0,
       help='The stop band attenuation(in dB) of the first stage filter(Normally negative).', metavar='dB')
 
-    parser.add_argument('--second-stage-pass-bw', type=float, default=40,
+    parser.add_argument('--second-stage-pass-bw', type=float, default=12,
        help='The pass bandwidth (in kHz) of the second stage filter '
           ' Starts at 0Hz and ends at this frequency', metavar='kHz')
-    parser.add_argument('--second-stage-stop-bw', type=float, default=47.999,
+    parser.add_argument('--second-stage-stop-bw', type=float, default=23.999,
        help='The stop bandwidth (in kHz) of the second stage filter.'
           ' Starts at 0Hz and ends at this frequency', metavar='kHz')
     parser.add_argument('--second-stage-stop-atten', type=float, default = -80.0,
@@ -249,8 +249,8 @@ def first_stage_output_coefficients(header, body, points, first_stage_num_taps, 
 
 def generate_second_stage(header, body, points,  pbw, sbw, second_stage_num_taps, stop_band_atten):
 
-  # This must reflect the output decimation ratio: 96/384 => 1/4
-  nulls = 1.0/4.0
+  # This must reflect the output decimation ratio: 48/384 => 1/8
+  nulls = 1.0/8.0
   a = [1, 0, 0]
   w = [1, 1, 1]
 
