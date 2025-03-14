d5618d879d78a41fea06946eebdfddb8f5830d0a
Richard Boulderstone <richard.boulderstone@gmail.com>
2025-03-14 09:35:50 +0000

    Changes to support improved low-pass filter for critical infrastructure

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_design.py
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_design.py
@@ -288,8 +288,8 @@ def generate_second_stage(header, body, points,  pbw, sbw, second_stage_num_taps
   #
   header.write(f"extern const int g_second_stage_fir32_critical_infrastructure[{len(coefs)//2}];\n")
   body.write("const int g_second_stage_fir32_critical_infrastructure[" + f"{len(coefs)//2}" + "] = {\n")
-  body.write("\t0x137F1F3, 0x15CA500, 0x1C8C6DB, 0x27910AA, 0x364FD6F, 0x484082B, 0x5C9BD44, 0x7285993,\n")
-  body.write("\t0x892C1B9, 0x9F93B42, 0xB4D5B44, 0xC80B6D6, 0xD8829E5, 0xE5740D6, 0xEE61E70, 0xF2EDCC6};\n")
+  body.write("\t0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,\n")
+  body.write("\t0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x07878787, 0x0F0F0F0E, 0x2D2D2D2A, 0x3C3C3C38};\n")
 
 
    # add our debugging data
