d00e2de6f1c50ed418bb198fe03848ad1c2bc105
Robert B. Calhoun <rcalhoun@shotspotter.com>
2025-11-12 15:33:53 +0000

    Cosmetic changes to filter definitions as generated by code in the previous commit

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
@@ -2,15 +2,23 @@
 extern const int g_first_stage_fir_0[256];
 extern const int g_first_stage_fir_1[256];
 extern const int g_first_stage_fir_2[256];
-extern const int fir1_debug[48];
+extern const int g_first_stage_fir_debug[48];
+
 #define FIRST_STAGE_MAX_PASSBAND_OUTPUT (619343346)
 
 extern const int g_second_stage_fir32[16];
+extern const int g_second_stage_fir32_debug[32];
 
 extern const int g_second_stage_fir32_critical_infrastructure[16];
 
 extern const int g_sine_wave3[128];
-extern const int g_crc_constants[2];
-extern const int fir2_debug[32];
 
+extern const int g_crc_constants[2];
+
+extern const int g_third_stage_fir_40kHz[16];
+extern const int g_third_stage_fir_32kHz[16];
+extern const int g_third_stage_fir_24kHz[16];
+extern const int g_third_stage_fir_16kHz[16];
+extern const int g_third_stage_fir_12kHz[16];
+extern const int g_third_stage_fir_8kHz[16];
 #define THIRD_STAGE_COEFS_PER_STAGE (32)
