2d2f888a3ac3e2f471f3ff8b954e16bea31b869e
Robert B. Calhoun <rcalhoun@shotspotter.com>
2026-02-05 05:16:58 +0000

    Makefile: bumprev to 2.9.2 with 48 kHz filter

--- 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
+++ 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
@@ -99,7 +99,7 @@
 
 #define BCD_DEVICE_J            2
 #define BCD_DEVICE_M            9
-#define BCD_DEVICE_N            0
+#define BCD_DEVICE_N            2
 
 #define AUDIO_CLASS             2
 #define AUDIO_CLASS_FALLBACK    0
