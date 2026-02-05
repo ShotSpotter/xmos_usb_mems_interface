5c9d5ab1216b7a144b3ccbe06907b386a4661b34
Robert B. Calhoun <rcalhoun@shotspotter.com>
2026-02-05 00:18:41 +0000

    fir_coefs.xc: Add new 31 tap coefficients
    The coefficients with lower frequency passbands were created with
    Remez algorithm; the high-bandwidth 48 kHz low-pass was creating
    using the window method with a Kaiser filter. See filter_design.py.

--- 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
+++ 01Firmware/PDM_USB/lib_mic_array/src/fir/fir_coefs.h
@@ -6,23 +6,20 @@ extern const int g_first_stage_fir_debug[48];
 
 #define FIRST_STAGE_MAX_PASSBAND_OUTPUT (619343346)
 
-extern const int g_second_stage_fir32_44kHz[16];
-extern const int g_second_stage_fir32_44kHz_debug[32];
+extern const int g_second_stage_fir31_48kHz[32];
+extern const int g_second_stage_fir31_48kHz_debug[32];
 
-extern const int g_second_stage_fir32_42kHz[16];
-extern const int g_second_stage_fir32_42kHz_debug[32];
+extern const int g_second_stage_fir31_44kHz[32];
+extern const int g_second_stage_fir31_44kHz_debug[32];
 
-extern const int g_second_stage_fir32_36kHz[16];
-extern const int g_second_stage_fir32_36kHz_debug[32];
+extern const int g_second_stage_fir31_36kHz[32];
+extern const int g_second_stage_fir31_36kHz_debug[32];
 
-extern const int g_second_stage_fir32_28kHz[16];
-extern const int g_second_stage_fir32_28kHz_debug[32];
+extern const int g_second_stage_fir31_30kHz[32];
+extern const int g_second_stage_fir31_30kHz_debug[32];
 
-extern const int g_second_stage_fir32_20kHz[16];
-extern const int g_second_stage_fir32_20kHz_debug[32];
-
-extern const int g_second_stage_fir32_12kHz[16];
-extern const int g_second_stage_fir32_12kHz_debug[32];
+extern const int g_second_stage_fir31_24kHz[32];
+extern const int g_second_stage_fir31_24kHz_debug[32];
 
 extern const int g_crc_constants[2];
 
