44fa0a5f0ff55c5901989b911a33c8d7b962e4b7
Robert B. Calhoun <rcalhoun@shotspotter.com>
2026-07-25 04:31:38 +0000

    Bumprev 3.0.10

--- 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
+++ 01Firmware/PDM_USB/PDM_USB/src/core/customdefines.h
@@ -99,7 +99,7 @@
 
 #define BCD_DEVICE_J            3
 #define BCD_DEVICE_M            0
-#define BCD_DEVICE_N            8
+#define BCD_DEVICE_N            10
 
 #define AUDIO_CLASS             2
 #define AUDIO_CLASS_FALLBACK    0
